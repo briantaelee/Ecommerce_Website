521f72aea2bf98d914fc3de238a58156cbeb7c84
briantaelee <briantae.lee95847@gmail.com>
2024-01-18 11:36:06 -0800

    finish

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Mikasa V200W",
+    price: 80.00,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Molten Flistatec",
+    price: 80.00,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Nike Knee Pads",
+    price: 29.99,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Bauerfeind Genutrain Knee Brace",
+    price: 99.00,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Athletic Tape",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Volleyball Net",
+    price: 299.00,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Spiking Training Aid",
+    price: 559.00,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Serve Machine",
+    price: 3499.00,
+    productImage: product8,
+  },
+];
